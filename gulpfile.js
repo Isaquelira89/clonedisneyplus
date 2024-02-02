2b020f317bad107443a2436eec63f45159a9c1b8
Isaque Lira <iskpdl@gmail.com>
2024-02-02 09:29:27 -0300

    hero and font add Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');    
 
-function styles() {
+function styles(done) {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .on('end', done);
+}
+function images(done) {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+        .on('end', done);
 }
 
-exports.default = styles;
+exports.default = gulp.parallel( styles, images);
 
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.parallel(images));
 }
